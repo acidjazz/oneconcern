b6f237ae51d3f86c00ca7b552a44b73b9f72d796
kevin olson <acidjazz@gmail.com>
2018-01-22 21:27:09 -0600

    ✨ successfull contentful integration, testing static generated build

--- pages/index.vue
+++ pages/index.vue
@@ -4,39 +4,36 @@
 </template>
 
 <script>
+
 import Carousel from '~/components/modules/Carousel.vue'
+
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
 export default {
   components: { Carousel },
-  data () {
-    return {
-      carousel: [
-        {
-          image: 'https://picsum.photos/1280/740?image=82',
-          title: 'Who We Are',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis neque massa, ac pulvinar elit gravida sit amet. Mauris volutpat feugiat venenatis. Pellentesque quis ipsum eros. ',
-          cta: { name: 'LEARN MORE', link: 'who', },
-        },
-        {
-          image: 'https://picsum.photos/1280/740?image=184',
-          title: 'What We Believe',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis neque massa, ac pulvinar elit gravida sit amet. Mauris volutpat feugiat venenatis. Pellentesque quis ipsum eros. ',
-          cta: { name: 'LEARN MORE', link: 'what', },
-        },
-        {
-          image: 'https://picsum.photos/1280/740?image=306',
-          title: 'Join the Team',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis neque massa, ac pulvinar elit gravida sit amet. Mauris volutpat feugiat venenatis. Pellentesque quis ipsum eros. ',
-          cta: { name: 'LEARN MORE', link: 'join', },
-        },
-        {
-          image: 'https://picsum.photos/1280/740?image=249',
-          title: 'Recent Updates',
-          description: 'Recent Updates dolor sit amet, consectetur adipiscing elit. Suspendisse convallis neque massa, ac pulvinar elit gravida sit amet. Mauris volutpat feugiat venenatis. Pellentesque quis ipsum eros. ',
-          cta: { name: 'LEARN MORE', link: 'updates', },
-        },
-      ],
+
+  async asyncData () {
+    let entries = await client.getEntries({'content_type': 'carousel'})
+    let carousel = []
+    for (let item of entries.items) { 
+      carousel.push({
+        image: item.fields.image.fields.file.url,
+        title: item.fields.title,
+        description: item.fields.description,
+        cta: {
+          name: item.fields.ctaName,
+          link: item.fields.ctaLink,
+        }
+      })
     }
-  },
+
+    return {
+      entries: entries,
+      carousel: carousel,
+    }
+
+  }
 
 }
 </script>
