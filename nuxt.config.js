48c4c33dec30f5f45fb3c950e0a663c522e948a0
kevin olson <acidjazz@gmail.com>
2018-01-23 02:53:46 -0600

    ✨ metadata, share image, GA, and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
 
+let config = {
+  title: 'One Concern',
+  description: 'Artificial Intelligence platform for Disasters',
+  url: 'https://oneconcern.com',
+  image: '/share.jpg',
+  keywords: 'key, words'
+}
+
 module.exports = {
 
   env: {
@@ -15,10 +23,38 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Artificial Intelligence platform for Disasters' }
+      { hid: 'description', name: 'description', content: 'Artificial Intelligence platform for Disasters' },
+      { name: 'theme-color', content: '#f29220' },
+
+      // Schema.org
+      { hid: 'itemprop:name', itemprop: 'name', content: config.title },
+      { hid: 'itemprop:description', itemprop: 'description', content: config.description },
+      { hid: 'itemprop:image', itemprop: 'image', content: config.url + config.image },
+
+      // facebook
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: config.url },
+      { hid: 'og:image', property: 'og:image', content: config.url + config.image },
+
+      { hid: 'og:title', property: 'og:title', content: config.title },
+      { hid: 'og:description', property: 'og:description', content: config.description },
+
+      // twitter
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:image', content: config.url + config.image },
+
+      { hid: 'twitter:title', name: 'twitter:title', content: config.title },
+      { hid: 'twitter:description', name: 'twitter:description', content: config.description },
+
     ],
+
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#f29220' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' },
     ]
   },
@@ -30,7 +66,10 @@ module.exports = {
   ** css frameowrk
   */
   css: [{ src: '~assets/stylus/main.styl', lang: 'stylus' }],
-  plugins: ['~/plugins/contentful.js'],
+  plugins: [
+    '~/plugins/contentful.js',
+    '~/plugins/ga.js',
+  ],
   /*
   ** Build configuration
   */
