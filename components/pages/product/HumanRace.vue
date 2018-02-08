f8ee3f9bef168424963ff3a2359e249460d91e90
kevin olson <acidjazz@gmail.com>
2018-02-08 10:23:29 -0600

    ✨ transitions and font tweaks

--- components/pages/product/HumanRace.vue
+++ components/pages/product/HumanRace.vue
@@ -9,12 +9,19 @@
       v-for="entry, index in copy",
       :class="{active: index === active}"
       @click="active = index") {{ entry.title }}
-  .body
-    .image(v-in-viewport)
-      img(:src="copy[active].image")
-    .copy
-      .subtitle(v-in-viewport) {{ copy[active].title }}
-      p(v-in-viewport) {{ copy[active].body }}
+    .content
+      transition(name="humanrace")
+        .body(
+          v-for="entry, index in copy",
+          v-if="index === active",
+          :key="index")
+          .image(
+            v-in-viewport,
+            :class="`image-${index}`")
+            img(:src="entry.image")
+          .copy
+            .subtitle(v-in-viewport) {{ entry.title }}
+            p(v-in-viewport) {{ entry.body }}
 </template>
 
 <script>
@@ -46,16 +53,16 @@ export default {
   text-align center
   color white
   .title
-    font-h4()
+    font-c4()
     inViewportBottom()
   .labels
     padding 30px
     .label
       inViewportBottom(0.1)
-      font-s1()
+      font-s2()
       display inline-block
       cursor pointer
-      width 200px
+      width 160px
       transition color 0.5s ease
       &:nth-child(1)
         border-right 1px solid white
@@ -64,23 +71,52 @@ export default {
         padding 0 0 0 10px
       &.active
         color fire-bush
-  .body
+  .content
+    margin 30px auto 0 auto
     width 900px
-    margin auto
-    display flex
-    .image
-      inViewportBottom(0.2)
-      img
-        width auto
-    .copy
-      width 240px
-      text-align left
-      .subtitle
-        color fire-bush
-        inViewportBottom(0.3)
-      p
-        font-s2()
-        inViewportBottom(0.4)
+    height 340px
+    overflow hidden
+    .body
+      width 900px
+      height 340px
+      overflow hidden
+      margin auto
+      display flex
+      .image
+        inViewportBottom(0.2)
+        width 470px
+        height 340px
+        &.image-0
+          img
+            width 470px
+        &.image-1
+          img
+            width 320px
+            margin-right 30px
+            float right
+      .copy
+        width 300px
+        text-align left
+        .subtitle
+          color fire-bush
+          inViewportBottom(0.3)
+          font-s6b()
+        p
+          font-s6()
+          inViewportBottom(0.4)
+
+.humanrace-enter-active
+  transition 0.4s ease 0.4s
+
+.humanrace-leave-active
+  transition 0.4s ease 0s
+
+.humanrace-enter, .humanrace-leave-to
+  opacity 0
+
+.humanrace-leave, .humanrace-enter-to
+  opacity 1
+
 
 @media all and (min-width: 1px) and (max-width: 1000px)
   #HumanRace
@@ -89,16 +125,22 @@ export default {
       .label
         font-s2()
         width 40%
-
-    .body
+    .content
       width auto
-      .image
-        display none
-        width 50%
-        > img
-          width 100%
-      .copy
+      height auto
+      .body
         width auto
-        padding 20px
+        .image
+          display none
+          width 50%
+          > img
+            width 100%
+        .copy
+          width auto
+          padding 20px
+          .subtitle.below-viewport,
+          p.below-viewport
+            opacity 1
+            transform translate(0, 0)
 
 </style>
