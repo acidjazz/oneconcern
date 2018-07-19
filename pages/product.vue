1c2b577d27fd998b682ef5725141f59a37c49bb2
Zivago Lee <z@ziff.net>
2018-07-19 14:04:42 -0700

    add ProductVideo

--- pages/product.vue
+++ pages/product.vue
@@ -4,6 +4,15 @@
     img.hero-background(:src="image")
     .hero-title {{ copy }}
   ScrollDown
+  .section.section-story(v-if="story")
+
+    .title(v-in-viewport.once) Our Story
+    .subsections
+      .subsection(v-in-viewport.once)
+        video(controls,:poster="story.poster")
+          source(:src="story.video",type="video/mp4")
+      .subsection(v-in-viewport.once)
+        p(v-for="block in storyCopy") {{ block }}
   .quote
     .copy(v-in-viewport.once) {{ copys.quoteTop }}
   HumanRace(:title="copys.titleHumanRace",:copy="copys.HumanRace")
@@ -44,6 +53,7 @@ export default {
   directives: { 'in-viewport': inViewportDirective },
   async asyncData () {
     const hero = await client.getEntries({ 'content_type': 'hero', 'fields.page': 'product'})
+    const story = await client.getEntries({'content_type': 'ProductVideo'})
     const text = await client.getEntries({ 'content_type': 'productCopy'})
     const HumanRaceEntries = await client.getEntries({ 'content_type': 'humanRace'})
     const BeforeAfterEntries = await client.getEntries({ 'content_type': 'beforeAfter', order: 'fields.order'})
@@ -84,12 +94,23 @@ export default {
       lowres: hero.items[0].fields.lowres.fields.file.url,
       image: hero.items[0].fields.image.fields.file.url,
       copy: hero.items[0].fields.copy,
+      story: {
+        copy: story.items[0].fields.storyCopy,
+        video: story.items[0].fields.storyVideo.fields.file.url,
+        poster: story.items[0].fields.storyPoster.fields.file.url,
+      },
       copys: copys,
     }
   },
   methods: {
     demo () {
       this.$store.commit('demo', true)
+    },
+  },
+
+  computed: {
+    storyCopy () {
+      return this.story.copy.split("\n")
     },
   },
 }
