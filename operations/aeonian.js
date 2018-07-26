c9cf987f2ca386d46904277457f54b3c4f2e4be9
kevin olson <acidjazz@gmail.com>
2018-07-26 14:46:04 -0500

    🔧 try switching to old cloudfront ids for now

--- operations/aeonian.js
+++ operations/aeonian.js
@@ -4,8 +4,16 @@ require('aeonian').config({
     prefix: '1c-'
   },
   environments: {
+
+    /* - official 1c id's
     production: 'E1Z577RFAY2GSN',
     staging: 'E1OCUBYNGDRQHE',
     development: 'CLOUDFRONTID',
+    */
+
+    // - dev id's
+    production: 'E26T22R6AY0TYO',
+    staging: 'E1C7VZR7WZRWBN',
+
   }
 }).deploy(process.argv[2])
