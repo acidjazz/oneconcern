42f80c26fb24778cdc5b9bf2038d77ff0e0fa5bb
kevin olson <acidjazz@gmail.com>
2019-03-21 12:40:03 -0500

    ✅ test aeonian upgrade

--- operations/aeonian.js
+++ operations/aeonian.js
@@ -5,16 +5,15 @@ require('aeonian').config({
   },
   environments: {
 
-    // - official 1c id's
+    /* - official 1c id's
     production: 'E1Z577RFAY2GSN',
     staging: 'E1OCUBYNGDRQHE',
     development: 'CLOUDFRONTID',
+    */
 
     // - dev id's
-    /*
     production: 'E26T22R6AY0TYO',
     staging: 'E1C7VZR7WZRWBN',
-    */
 
   }
 }).deploy(process.argv[2])
