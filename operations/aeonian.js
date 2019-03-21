c5f38cdd67dffcf96db8c2464045b0a7bf34d51e
kevin olson <acidjazz@gmail.com>
2019-03-21 13:20:50 -0500

    🔧 testing aeonian v1.1.7 bugfix

--- operations/aeonian.js
+++ operations/aeonian.js
@@ -7,15 +7,15 @@ require('aeonian').config({
   },
   environments: {
 
-    // - official 1c id's
+    /* - official 1c id's
     production: 'E1Z577RFAY2GSN',
     staging: 'E1OCUBYNGDRQHE',
     development: 'CLOUDFRONTID',
+    */
 
-    /* - dev id's
+    // - dev id's
     production: 'E26T22R6AY0TYO',
     staging: 'E1C7VZR7WZRWBN',
-    */
 
   }
 }).deploy(process.argv[2])
