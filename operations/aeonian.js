0adf7bc47473423d32b6d18fed0d0532196df612
kevin olson <acidjazz@gmail.com>
2019-03-21 12:58:13 -0500

    ✅ testing region and remove_old aeonian config

--- operations/aeonian.js
+++ operations/aeonian.js
@@ -1,20 +1,21 @@
 require('aeonian').config({
   bucket: {
     localDir: './dist/',
-    prefix: '1c-'
+    prefix: '1c-',
+    region: 'us-east-1',
+    remove_old: false,
   },
   environments: {
 
-    // - official 1c id's
+    /* - official 1c id's
     production: 'E1Z577RFAY2GSN',
     staging: 'E1OCUBYNGDRQHE',
     development: 'CLOUDFRONTID',
+    */
 
     // - dev id's
-    /*
     production: 'E26T22R6AY0TYO',
     staging: 'E1C7VZR7WZRWBN',
-    */
 
   }
 }).deploy(process.argv[2])
