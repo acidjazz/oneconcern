ac34092f4091134e21b2006a6e7ebead16a9e469
Kevin Olson <acidjazz@gmail.com>
2020-06-29 17:47:23 -0500

    🔧 put the routes back for sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ let config = {
   keywords: 'key, words'
 }
 
-// let routes = require('./static/cache/routes.json')
+let routes = require('./static/cache/routes.json')
 
 module.exports = {
 
@@ -114,7 +114,7 @@ module.exports = {
   sitemap: {
     hostname: 'https://oneconcern.com',
   },
-  // generate: { routes },
+  generate: { routes },
   build: {
     /*
     ** Run ESLint on save
